72653cc6eeebe7eef76b93dfd74e6c94491a7220
Blake Lin <xblake0531@gmail.com>
2023-11-01 01:47:04 +0800

    Add new file 123.py

---
+++ 123.py
@@ -0,0 +1,11 @@
+price = 100
+circle = 3.14
+sun = False
+schedule = 'Can not match carrier request'
+
+print(price)
+print(circle)
+print(sun)
+print(schedule)
+
+print(price, circle, sun, schedule)
