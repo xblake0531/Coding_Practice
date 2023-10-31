a37bbea45ca04f7d59377584dcbf5dfdc47bef9b
Blake Lin <xblake0531@gmail.com>
2023-11-01 01:52:12 +0800

    Add new file hi.py

---
+++ hi.py
@@ -0,0 +1,8 @@
+name = input('請問你叫什麼名子?')
+print ('你的名子是,', name)
+height = input('What is your height(cm)?')
+print('Your height is ', height, 'cm')
+weight = input('What is your weight(kg)?')
+print('Your weight is ', weight, 'kg')
+
+print('Your name is ', name, ', your height is', height, 'cm', ' and your weight is ', weight, 'kg')
