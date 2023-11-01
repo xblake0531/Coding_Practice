a9a11176c1f8770c6b4f2b7e63bbd8801b7214ea
Blake Lin <xblake0531@gmail.com>
2023-11-02 00:28:46 +0800

    Add new file condition.py

---
+++ condition.py
@@ -0,0 +1,15 @@
+x = 100
+
+print(x) # 100
+print(x == 100) # True
+print(x == 60) # False
+print(x != 100) # False
+print(x != 60) # True
+print(x > 60) # True
+print(x >= 60) # True
+print(x > 100) # False
+print(x >= 100) # True
+print(x < 60) # False
+print(x <= 60) # False
+print(x < 100) # False
+print(x <= 100) # True
